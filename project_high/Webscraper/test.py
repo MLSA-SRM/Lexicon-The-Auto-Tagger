9213cd8f61a46f3cfb2dac26a4b3d76f7a5cc913
Mihir Singh <mihirs16@gmail.com>
2020-02-03 14:11:38 +0530

    Update Test Logs

--- project_high/Webscraper/test.py
+++ project_high/Webscraper/test.py
@@ -81,4 +81,4 @@ def scrapeURL(url):
 medium_tags_df = pd.read_csv('C:/Users/Powerhouse/Desktop/Project High/Data/medium-tag-list-dataset/medium_tag_1000.csv')
 tag_list = list(medium_tags_df['Tags'])
 
-print(scrapeURL('https://medium.com/i-math/the-binomial-theorem-explained-6464f41e5268'))
+print(scrapeURL('https://towardsdatascience.com/explainable-deep-learning-in-breast-cancer-prediction-ae36c638d2a4'))
