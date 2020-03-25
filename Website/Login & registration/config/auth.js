0bb5148c22b6d4669c2ed0c10ae195ec750a6c9c
AayushMehta169 <52970754+AayushMehta169@users.noreply.github.com>
2020-03-25 12:58:38 +0530

    Clean Up

--- Website/Login & registration/config/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports ={
-    ensureAuthenticated: function(req, res, next){
-        if( req.isAuthenticated()){
-            return next();
-        }
-        req.flash('error_msg', 'Please login to view this page');
-        res.redirect('/users/login');
-    }
-}
