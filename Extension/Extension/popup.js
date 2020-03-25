345487df01a75196b3fc0fab8287ac58f37ee805
AayushMehta169 <52970754+AayushMehta169@users.noreply.github.com>
2020-03-25 16:51:48 +0530

    Tag return

--- Extension/Extension/popup.js
+++ Extension/Extension/popup.js
@@ -4,12 +4,25 @@ function Disapp(){
     tagHead.style.display = "block"
 }
 function postTest() {   
+    var result;
     chrome.tabs.executeScript({
         file: 'testRun.js'
     });
     chrome.runtime.onMessage.addListener(function(msg, sender, response){
-        document.write(msg.tags);
-    })
+        result = msg.tags;
+        
+        function appendData(data){
+            var mainContainer = document.getElementById("incTags");
+            for (var i = 0; i < data.length; i++) {
+                var div = document.createElement("button");
+                div.classList.add("tagStyle"); 
+                div.innerHTML =  data[i];
+                mainContainer.appendChild(div);
+              }
+            }
+        appendData(result);
+    });
+    
 }
 
 document.getElementById("post-test").addEventListener("click", postTest);
